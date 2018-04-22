d196e1fb82faaf47158a99481ecc88d659b4c107
hongzicong <973138379@qq.com>
2018-04-22 13:45:56 +0800

    oh fuck fuck

--- projects/secondProject/SparseGrid/SparseGridRunner.java
+++ projects/secondProject/SparseGrid/SparseGridRunner.java
@@ -1,9 +1,8 @@
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.ActorWorld;
 import info.gridworld.grid.Location;
-import info.gridworld.actor.Critter;
-import info.gridworld.actor.Rock;
-import info.gridworld.actor.Flower;
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.CrabCritter;
 /**
  * This class runs a world with additional grid choices.
  */
@@ -22,7 +21,8 @@ public final class SparseGridRunner
         world.addGridClass("UnboundedGrid2");
 
         // Init a new Critter
-        world.add(new Location(2, 2), new Critter());
+        world.add(new Location(2, 2), new Bug());
+        world.add(new Location(5, 5), new Bug());
 
         world.show();
     }
