e61d266fd4914b6949c3b0cfe5bbf99eb98da387
hongzicong <973138379@qq.com>
2018-05-05 09:24:06 +0800

    fightgit add .!

---
+++ projects/thirdProject/MazeBug/mazeBug/info/gridworld/maze/MazeBug.java
@@ -0,0 +1,292 @@
+package info.gridworld.maze;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.*;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
+/**
+ * A <code>MazeBug</code> can find its way in a maze. <br />
+ * The implementation of this class is testable on the AP CS A and AB exams.
+ */
+public class MazeBug extends Bug {
+	
+	public Location next;
+	
+	public boolean isEnd = false;
+	
+	public Stack<ArrayList<Location>> crossLocation = new Stack<ArrayList<Location>>();
+
+	public Integer stepCount = 0;
+	
+	// final message has been shown
+	boolean hasShown = false; 
+
+	// East, South, West, North
+	private int[] directProb = {1, 1, 1, 1};
+
+	/**
+	 * denotes to the position of each direction in directProb
+	 * used to solve magic number problem
+	 */
+	private static final int EAST_PROB = 0;
+	private static final int SOUTH_PROB = 1;
+	private static final int WEST_PROB = 2;
+	private static final int NORTH_PROB = 3;
+
+	/**
+	 * Constructs a box bug that traces a square of a given side length
+	 * 
+	 * @param length
+	 *            the side length
+	 */
+	public MazeBug() {
+		setColor(Color.GREEN);
+		last = new Location(0, 0);
+	}
+
+	/**
+	 * Moves to the next location of the square.
+	 */
+	public void act() {
+
+		// init
+		if(stepCount == 0){  
+			Location local = getLocation();  
+
+			// push first road into the crossLocation record
+			ArrayList<Location> firstList = new ArrayList<Location>();  
+			firstList.add(local);
+			crossLocation.push(firstList);
+
+			// init the probability
+			guessDirectProb();
+		}
+
+		boolean willMove = canMove();
+
+		if (isEnd == true) {
+		//to show step count when reach the goal
+			if (hasShown == false) {
+				String msg = stepCount.toString() + " steps";
+				JOptionPane.showMessageDialog(null, msg);
+				hasShown = true;
+			}
+		} else if (willMove) {
+			// can move to next position
+			move();
+			//increase step count when move 
+			stepCount++;
+		} else {
+			backTrack();
+		}
+	}
+
+	/**
+	 * Find all positions that can be move to.
+	 * 
+	 * @param loc
+	 *            the location to detect.
+	 * @return List of positions.
+	 */
+	public ArrayList<Location> getValid(Location loc) {
+		Grid<Actor> gr = getGrid();
+		if (gr == null){
+			return null;
+		}
+		ArrayList<Location> valid = new ArrayList<Location>();
+		Location[] allLoc = {loc.getAdjacentLocation(Location.EAST), loc.getAdjacentLocation(Location.WEST),
+							 loc.getAdjacentLocation(Location.SOUTH), loc.getAdjacentLocation(Location.NORTH)};
+		
+		for(Location tempLoc:allLoc){
+			if(gr.isValid(tempLoc)){
+				Actor actor = gr.get(tempLoc);
+				if(actor instanceof Rock && actor.getColor().equals(Color.RED)){
+					next = tempLoc;
+					ArrayList<Location> tempArr = new ArrayList<>();
+					tempArr.add(tempLoc);
+					return tempArr;
+				} else if(actor == null){
+					valid.add(tempLoc);
+				}
+			}
+		}
+
+		return valid;
+	}
+
+	/**
+	 * Tests whether this bug can move forward into a location that is empty or
+	 * contains a flower.
+	 * 
+	 * @return true if this bug can move.
+	 */
+	public boolean canMove() {
+
+		Grid<Actor> gr = getGrid();
+		// check whether the next location is valid or not 
+		if(!gr.isValid(next)){
+			return false;
+		}
+
+		// check if the next location can move
+		if(getValid(getLocation()).size() == 0){
+			return false;
+		}
+
+		// the next location is a Rock which is not red
+		return true;
+	}
+
+	/**
+	 * Moves the bug forward, putting a flower into the location it previously
+	 * occupied.
+	 * Meanwhile choose a new next position
+	 */
+	public void move() {
+
+		Grid<Actor> gr = getGrid();
+
+		if (gr == null)
+			return;
+ 
+		Location loc = getLocation();
+
+		// change next location and last location
+		ArrayList<Location> canMovePosList = getValid(loc);
+
+		int directPos = 0;
+		next = getNextPos(canMovePosList);
+
+		if (g.isValid(next)) {  
+            Actor actor = gr.get(next);  
+              
+            if(actor instanceof Rock && actor.getColor().equals(Color.RED) ){  
+                isEnd = true; 
+			}  
+			// It may be end if it reaches to red rock instead of only facing the red rock
+            moveTo(next);  
+            int facing = loc.getDirectionToward(next);  
+            this.setDirection(facing);  
+              
+            ArrayList<Location> temp = crossLocation.pop();  
+            temp.add(next);  
+            crossLocation.push(temp);  
+              
+            ArrayList<Location> latest = new ArrayList<Location>();  
+            latest.add(next);  
+            crossLocation.push(latest);  
+        } else {  
+            removeSelfFromGrid();  
+        }
+
+		leaveFlower(loc);
+	}
+
+	/**
+	 * used to guess the radom probability of each direction
+	 */
+	public void guessDirectProb(){  
+
+		Grid<Actor> gr = getGrid();  
+		ArrayList<Location> locations = gr.getOccupiedLocations();  
+			
+		for(Location loc : locations){  
+			Actor goal = gr.get(loc);  
+			if (goal instanceof Rock && goal.getColor().equals(Color.RED)){  
+				// bug in north of goal, so it needs to go south
+				if(getLocation().getRow() < goal.getRow()){  
+					probility[SOUTH_PROB] += 5;  
+				// bug in south of goal, so it needs to go north
+				} else { 
+					probility[NORTH_PROB] += 5;  
+				} 
+				// bug in west of goal, so it needs to go east
+				if(getLocation().getCol() < goal.getCol()){  
+					probility[EAST_PROB] += 5;
+				// bug in east of goal, so it needs to go west
+				}else {  
+					probility[WEST_PROB] += 5;  
+				}  
+				return;  
+			} 
+		}  
+	}
+
+
+	public void backTrack(){
+		if(crossLocation.size() != 0){
+
+			crossLocation.pop();
+
+			if(crossLocation.size() != 0){
+				Grid gr = getGrid();
+				
+				ArrayList<Location> backRoad = crossLocation.peek();  
+				Location oldLoc = backRoad.get(0);
+				
+				Location loc = getLocation();
+
+				int direction = loc.getDirectionToward(oldLoc);
+
+				if (gr.isValid(returnLocation)) {  
+                    setDirection(direction);  
+                    moveTo(oldLoc);  
+                    stepCount ++;
+                } else {  
+                    removeSelfFromGrid();  
+				}
+				
+				if ((int)(direction / 90) == Location.NORTH ) {  
+                    probility[SOUTH_PROB] --;  
+                } else if ((int)(direction / 90) == Location.EAST ) {  
+                    probility[WEST_PROB] --;  
+                } else if ((int)(direction / 90) == Location.SOUTH ) {  
+                    probility[NORTH_PROB] --;  
+                } else if ((int)(direction / 90) == Location.WEST ) {  
+                    probility[EAST_PROB] --;  
+				}  
+				
+				leaveFlower(loc);
+			}
+		}
+	}
+
+	// leave flower after the bug
+	private void leaveFlower(Location loc){
+		Flower flower = new Flower(getColor());  
+		flower.putSelfInGrid(getGrid(), loc);
+	}
+
+	private Location getNextPos(ArrayList<Location> validLoc){
+		
+		ArrayList<Integer> tempDirect = new ArrayList<>();
+
+		for(Location tempLoc:validLoc){
+
+		}
+		
+		int randRange = r.nextInt(100);
+		
+		int m; //结果数字
+		
+		if(n5 < 55){ //55个数字的区间，55%的几率
+			m = 1;
+		}else if(n5 < 95){//[55,95)，40个数字的区间，40%的几率
+			m = 2;
+		}else{
+			m = 3;
+		}
+
+		return result;
+	}
+
+}
